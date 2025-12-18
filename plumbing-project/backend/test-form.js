a8c694174873a55fdf8282467da180a3a7841560
steffiag <steffiageorge@gmail.com>
2025-12-17 20:08:55 -0500

    going back to j one repo

--- plumbing-project/backend/test-form.js
+++
@@ -1,8 +0,0 @@
-import { getFormResponses } from "./google-forms.js";
-
-const formId = "1K2a3Akdr75XpojqzEyMLBP9IHx4fS6lMZYtRps-ngZo";
-
-(async () => {
-  const responses = await getFormResponses(formId);
-  console.log(responses);
-})();
